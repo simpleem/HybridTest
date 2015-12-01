b71aa9e3d55b25825e2c27734df30b75a94986f8
simpleem <simpleem@simpleemdemac-mini.local>
2015-12-01 09:20:03 +0800

    Initial Commit

---
+++ SSTextHtmlCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSTextHtmlCache
+//
+//  Created by simpleem on 15/12/1.
+//  Copyright © 2015年 simpleem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SSTextHtmlCache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSTextHtmlCache
+//
+//  Created by simpleem on 15/12/1.
+//  Copyright © 2015年 simpleem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
